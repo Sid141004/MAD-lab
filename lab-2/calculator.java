859c15fc4592178a68e217dbcbdedbc025e2ed26
siddharth <144979580+Sid141004@users.noreply.github.com>
2025-01-29 16:10:28 +0530

    Update calculator.java

--- lab-2/calculator.java
+++ lab-2/calculator.java
@@ -1,5 +1,6 @@
 package com.example.firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +14,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class calculator extends AppCompatActivity {
 
-    private Button one, two, three, four, five, six, seven, eight, nine, zero, add, multiply, divide, subtract, mod, equal, clear, back, dot;
-    private String temp="";
+    private Button one, two, three, four, five, six, seven, eight, nine, zero, add, multiply, divide, subtract, mod, equal, clear, back, dot, exit;
+    private char op='+';
+    private String res1="";
+    private String res2="";
     private TextView main;
+
+    void eval(){
+        res1 = "";
+        res2 = "";
+        int flag = 0;
+        String curr = main.getText().toString();
+
+        for (char c : curr.toCharArray()) {
+            if ((c >= '0' && c <= '9') || c == '.') {
+                if (flag == 0) {
+                    res1 += c;
+                } else {
+                    res2 += c;
+                }
+            } else if (c == '+' || c == '-' || c == 'x' || c == '/' || c == '%') {
+                flag = 1;
+                op = c;
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,24 +72,20 @@ public class calculator extends AppCompatActivity {
         clear = (Button) findViewById(R.id.clear);
         back = (Button) findViewById(R.id.back);
         dot = (Button) findViewById(R.id.dot);
+        exit = (Button) findViewById(R.id.none);
 
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "1";
                 main.setText(temp);
-//                if(op1 != ""){
-//                    op1 += "1";
-//                    main.setText(op1);
-//                }else if (op2 != ""){
-//                    op2 += "1";
-//                    main.setText(op2);
-//                }
             }
         });
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "2";
                 main.setText(temp);
             }
@@ -73,6 +93,7 @@ public class calculator extends AppCompatActivity {
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "3";
                 main.setText(temp);
             }
@@ -80,6 +101,7 @@ public class calculator extends AppCompatActivity {
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "4";
                 main.setText(temp);
             }
@@ -87,6 +109,7 @@ public class calculator extends AppCompatActivity {
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "5";
                 main.setText(temp);
             }
@@ -94,6 +117,7 @@ public class calculator extends AppCompatActivity {
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "6";
                 main.setText(temp);
             }
@@ -101,6 +125,7 @@ public class calculator extends AppCompatActivity {
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "7";
                 main.setText(temp);
             }
@@ -108,6 +133,7 @@ public class calculator extends AppCompatActivity {
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "8";
                 main.setText(temp);
             }
@@ -115,6 +141,7 @@ public class calculator extends AppCompatActivity {
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "9";
                 main.setText(temp);
             }
@@ -122,8 +149,109 @@ public class calculator extends AppCompatActivity {
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String temp = main.getText().toString();
                 temp += "0";
                 main.setText(temp);
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main.setText("");
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n = main.getText().toString().length();
+                String curr = main.getText().toString().substring(0, n-1);
+                main.setText(curr);
+            }
+        });
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eval();
+                if (res1.isEmpty() || res2.isEmpty()) {
+                    main.setText("Error");
+                    return;
+                }
+
+                double a = Double.parseDouble(res1);
+                double b = Double.parseDouble(res2);
+                String res = "";
+
+                switch (op) {
+                    case '+': res = Double.toString(a + b); break;
+                    case '-': res = Double.toString(a - b); break;
+                    case 'x': res = Double.toString(a * b); break;
+                    case '/': res = (b != 0) ? Double.toString(a / b) : "Error"; break;
+                    case '%': res = (b != 0) ? Double.toString(a % b) : "Error"; break;
+                }
+
+                if (res.endsWith(".0")) {
+                    res = res.substring(0, res.length() - 2);
+                }
+
+                main.setText(res);
+
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = main.getText().toString();
+                temp += "+";
+                main.setText(temp);
+            }
+        });
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = main.getText().toString();
+                temp += "-";
+                main.setText(temp);
+            }
+        });
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = main.getText().toString();
+                temp += "x";
+                main.setText(temp);
+            }
+        });
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = main.getText().toString();
+                temp += "/";
+                main.setText(temp);
+            }
+        });
+        mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = main.getText().toString();
+                temp += "%";
+                main.setText(temp);
+            }
+        });
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = main.getText().toString();
+
+                if (!temp.contains(".")) {
+                    temp += ".";
+                    main.setText(temp);
+                }
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(calculator.this, MainActivity.class));
             }
         });
 
